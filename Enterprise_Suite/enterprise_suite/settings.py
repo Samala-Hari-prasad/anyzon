dbb095e3476b80658d85a35aad0110d26c7cbfff
Hari <hariprasad8587@gmail.com>
2026-02-26 09:34:01 +0530

    Configure Creative Hub and Enterprise Suite for Vercel deployment

--- Enterprise_Suite/enterprise_suite/settings.py
+++ Enterprise_Suite/enterprise_suite/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,8 +132,9 @@ CURRENCY_SYMBOL = '₹'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
